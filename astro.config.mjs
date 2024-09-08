92f76e24e20f7a01f5a493b4425a8dffc1c8aaa8
Andrés Felipe Rodríguez Gallego <79536152+andres-rod-g@users.noreply.github.com>
2024-09-07 19:39:19 -0500

    Now can build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import 'dotenv/config'
+import node from "@astrojs/node";
+import 'dotenv/config';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  output: 'hybrid',
+  output: 'server',
+  adapter:  node({
+    mode: "middleware"
+  }),
   integrations: [mdx(), sitemap(), tailwind(), icon(), react()],
   server: {
     port: parseInt(process.env.PORT) || 4321, host: true
