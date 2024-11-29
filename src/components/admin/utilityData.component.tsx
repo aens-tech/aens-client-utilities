b74c04fc8d4ef240f104d94e124708837a759d1f
Andrés Felipe Rodríguez Gallego <79536152+andres-rod-g@users.noreply.github.com>
2024-11-28 19:56:27 -0500

    Last updates

--- src/components/admin/utilityData.component.tsx
+++ src/components/admin/utilityData.component.tsx
@@ -1,21 +1,24 @@
 import { API_URL } from "@/lib/utils/env";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BarChart } from "../tremor/components/barChar";
 import { IContactActionData, IContactActionGraphData } from "@/interfaces/models/contactAction.model";
 import ContactCardComponent from "./utilityData/contactCard.component";
 import { Button } from "@nextui-org/button";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import {
-  Modal, 
-  ModalContent, 
-  ModalHeader, 
-  ModalBody, 
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
   ModalFooter,
   useDisclosure
 } from "@nextui-org/modal";
 import { IUtility, UTILITY_TYPES } from "@/interfaces/utility";
 import API from "@/API/API";
 import { Spinner } from "@nextui-org/react";
+
+import generatePDF from 'react-to-pdf';
+import { download, generateCsv, mkConfig } from "export-to-csv";
 
 interface CustomProps {
   slug: string;
@@ -31,6 +34,7 @@ const UtilityDataComponent: React.FC<CustomProps> = ({
   slug,
   type
 }) => {
+  const pdfRef = useRef<HTMLDivElement>(null)
 
   const [contactActionData, setContactActionData] = useState<IContactActionAnswer | null>(null)
   const [stateFetch, setStateFetch] = useState<'idle' | 'isLoading' | 'succeeded' | 'error'>('idle');
@@ -86,22 +90,46 @@ const UtilityDataComponent: React.FC<CustomProps> = ({
   }
 
   const handleRemoveUtility = async () => {
-      setUtilityFetchState("LOADING")
+    setUtilityFetchState("LOADING")
 
     const temp = await API.deleteUtility(slug)
 
     if (temp.status !== 200) {
-        setUtilityFetchState('ERROR');
+      setUtilityFetchState('ERROR');
       return
     }
 
-      setUtilityFetchState("SUCCEDED")
+    setUtilityFetchState("SUCCEDED")
   }
 
   useEffect(() => {
     handleGetConvocatoria()
     handleFetchUtils()
   }, [])
+
+  const generateExcel = () => {
+    const csvConfig = mkConfig({ useKeysAsHeaders: true });
+
+    // Converts your Array<Object> to a CsvOutput string based on the configs
+    // @ts-ignore
+    const csv = generateCsv(csvConfig)([...contactActionData!.data.map((v) => {
+
+      v = {
+        ...v,
+        ...v.user,
+        // @ts-ignore
+        interests: v.user.interests.join(',')
+      }
+
+      // @ts-ignore
+      v.user = undefined
+      return v
+    }
+    )] ?? []);
+
+    // Get the button in your HTML
+    download(csvConfig)(csv)
+  }
 
   const renderer = () => {
     switch (utilityFetchState) {
@@ -178,47 +206,47 @@ const UtilityDataComponent: React.FC<CustomProps> = ({
                 startContent={
                   <Icon icon={"solar:document-bold"} fontSize={25} />
                 }
+                onClick={() => generatePDF(pdfRef, { filename: 'page.pdf' })}
               >PDF
               </Button>
             </a>
-            <a
-              className=" w-full"
-              href={`/${utility!.type === UTILITY_TYPES.DOWNLOAD ? "descarga" : "convocatoria"}/${slug}`}
-            >
-              <Button
-                color="primary"
-                className=" text-white w-full"
-                startContent={
-                  <Icon icon={"solar:widget-bold"} fontSize={25} />
-                }
-              >Excel
-              </Button>
-            </a>
+            <Button
+              color="primary"
+              className=" text-white w-full"
+              startContent={
+                <Icon icon={"solar:widget-bold"} fontSize={25} />
+              }
+              onClick={() => generateExcel()}
+            >Excel
+            </Button>
           </div>
-          {
-            contactActionData?.graphData &&
-              contactActionData.graphData.length > 0
-              ?
-              <BarChart
-                className="h-80"
-                data={contactActionData?.graphData.toReversed()}
-                index="_id"
-                categories={['cantidad']}
-              />
-              : <div className=" flex h-80 w-full animate-pulse bg-default-200 rounded-md items-center justify-center text-default-500">No se encontraron datos para mostrar.</div>
-          }
-          {
-            contactActionData?.data &&
-              contactActionData.data.length > 0
-              ?
-              <div className=" flex flex-col w-full space-y-2">
-                {
-                  contactActionData.data.map((v) => <ContactCardComponent contactActionData={v} />)
-                }
-              </div>
-              :
-              <p className=" text-default-700 font-light">Aun nadie ha usado esta utilidad.</p>
-          }
+          <div ref={pdfRef}>
+
+            {
+              contactActionData?.graphData &&
+                contactActionData.graphData.length > 0
+                ?
+                <BarChart
+                  className="h-80"
+                  data={contactActionData?.graphData.toReversed()}
+                  index="_id"
+                  categories={['cantidad']}
+                />
+                : <div className=" flex h-80 w-full animate-pulse bg-default-200 rounded-md items-center justify-center text-default-500">No se encontraron datos para mostrar.</div>
+            }
+            {
+              contactActionData?.data &&
+                contactActionData.data.length > 0
+                ?
+                <div className=" flex flex-col w-full space-y-2">
+                  {
+                    contactActionData.data.map((v) => <ContactCardComponent contactActionData={v} />)
+                  }
+                </div>
+                :
+                <p className=" text-default-700 font-light">Aun nadie ha usado esta utilidad.</p>
+            }
+          </div>
           {/* <pre>
         {JSON.stringify(contactActionData, null, 2)}
       </pre> */}
